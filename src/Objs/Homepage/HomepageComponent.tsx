ef5bd084862c90072f7d54b42fe50752c5e0d673
Markus Hartig <markus.hartig@justrelate.com>
2025-10-15 16:14:20 +0200

    TMP test ai-assistant

--- src/Objs/Homepage/HomepageComponent.tsx
+++ src/Objs/Homepage/HomepageComponent.tsx
@@ -1,6 +1,8 @@
 import { ContentTag, provideComponent } from 'scrivito'
 import { Homepage } from './HomepageObjClass'
+import { aiAssistant } from '@justrelate/ai-assistant'
 
-provideComponent(Homepage, ({ page }) => (
-  <ContentTag tag="main" id="main" content={page} attribute="body" />
-))
+provideComponent(Homepage, ({ page }) => {
+  aiAssistant({ botId: '30dd326edd5477b0' })
+  return <ContentTag tag="main" id="main" content={page} attribute="body" />
+})
